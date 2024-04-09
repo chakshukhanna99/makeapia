b1e191e79b73d07db1cad1cbde31dde4b6d685b8
Chakshu Khanna <chakshukhanna2110@gmail.com>
2024-04-09 09:22:56 +0530

    port added

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ require("dotenv").config();
 const express = require('express');
 const app = express();
 const connectdb = require('./db/connect')
-const PORT = process.env.PORT || 1000;
+const PORT = process.env.PORT || 3000; // Change the port to 3000 or any other available port
+
 const products_routes = require('./routes/products')
 app.get("/",(req,res)=>{
     res.send("hi i am live")
@@ -11,7 +12,7 @@ app.use("/api/products",products_routes);
 const start = async ()=>{
     try{
         await connectdb(process.env.MONGODB_URL);
-       app.listen(PORT,()=>{
+       app.listen(PORT,"0.0.0.0",()=>{
           console.log("Server is running on port " + PORT);
         });
     }
